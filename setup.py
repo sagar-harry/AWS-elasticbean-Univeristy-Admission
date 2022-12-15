f1658a8ede34780dd88b03680da5986062f4c1f6
Konni Vidya Sagar <RH0957@accionlabs.in>
2022-12-15 15:06:52 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using various tools to structure DS projects in a better way',
+    author='Konni Vidya Sagar',
+    license='MIT',
+)
