9dbb588288531185f139d0845914408be8349092
Konni Vidya Sagar <RH0957@accionlabs.in>
2022-12-19 12:39:45 +0530

    Model training and saving completed

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 
+from sklearn.linear_model import Ridge
+from sklearn.metrics import r2_score
+import joblib
 
 def retrieve_params(config):
     with open(config) as yaml_file:
@@ -12,8 +15,7 @@ def retrieve_params(config):
     return params
 
 
-def trainTestSplit(config):
-    params = retrieve_params(config=config)
+def trainTestSplit(params):
     df = pd.read_csv(params["data"]["processed_data"])
     X_train, X_test, y_train, y_test = train_test_split(
                                         df.iloc[:,:-1], df.iloc[:,-1], 
@@ -39,12 +41,31 @@ def standardizing(X_train, X_test):
     return X_train, X_test
 
 def train_model(config):
-    X_train, X_test, y_train, y_test = train_test_split(config)
-    X_train, X_test = standardizing(X_train, X_test)
+    params = retrieve_params(config=config)
+
+    X_train, X_test, y_train, y_test = trainTestSplit(params)
+    X_train_standardized, X_test_standardized = standardizing(X_train, X_test)
+    model1 = Ridge(alpha=params["model_training"]["model_params"]["Ridge"]["alpha"])
+    model1.fit(X_train_standardized, y_train)
+
+    print(r2_score(y_test, model1.predict(X_test_standardized)))
+    print(r2_score(y_train, model1.predict(X_train_standardized)))
+
+    with open(config, "r") as my_file:
+        my_dict = yaml.safe_load(my_file)
+        my_dict["model_training"]["model_metrics"]["testing_r2_score"] = float(r2_score(y_test, model1.predict(X_test_standardized)))
+        my_dict["model_training"]["model_metrics"]["training_r2_score"] = float(r2_score(y_train, model1.predict(X_train_standardized)))
+
+    with open(config, "w") as my_file2:
+        yaml.dump(my_dict, my_file2, allow_unicode=True, indent=4)
+
+    params = retrieve_params(config=config)
+    joblib.dump(model1, params["model_training"]["model_location"])
+    
 
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    trainTestSplit(parsed_args.config)
+    train_model(parsed_args.config)
