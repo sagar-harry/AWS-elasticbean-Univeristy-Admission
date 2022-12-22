52b408250ff2635fa48aa33f6da616bdff884825
sagar-harry <10vidyasagarkonni@gmail.com>
2022-12-22 16:17:38 +0530

    Addded validation of inputs and outputs in the prediction stage

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -12,8 +12,36 @@ def retrieve_params(config):
 
 params = retrieve_params(config="params.yaml")
 
+class InputException(Exception):
+    def __init__(self, attribute, range):
+        self.attribute = attribute
+        self.range = range
+        self.message = f"{self.attribute} : Input values are not in specified range of {range}"
+        super().__init__(self.message)
+
+def validate_input(data):
+    schema_file = params["form_data"]["schema_json"]
+    with open(schema_file) as json_file:
+        schema_dict = json.load(json_file)
+
+    for i in data.keys():
+        if "min" in schema_dict[i].keys():
+            if not schema_dict[i]["min"] <= data[i] <= schema_dict[i]["max"]: 
+                raise InputException(i, schema_dict[i])
+        else:
+            if not data[i] in schema_dict[i]["values"]:
+                raise InputException(i, schema_dict[i])
+
+def validate_output(output):
+    schema_file = params["form_data"]["schema_json"]
+    with open(schema_file) as json_file:
+        schema_dict = json.load(json_file)
+
+    if not schema_dict["output"]["min"] <= output <= schema_dict["output"]["max"]:
+        raise Exception("Output not in range")
 
 def predict(data):
+    validate_input(data)
     cgpa_scaler = joblib.load(
         params["model_training"]["standard_scaler_location"]["cgpa_scaler"])
     gre_scaler = joblib.load(
@@ -23,9 +51,10 @@ def predict(data):
         np.array(data["GRE Score"]).reshape(-1, 1))[0][0]
     data["CGPA"] = cgpa_scaler.transform(
         np.array(data["CGPA"]).reshape(-1, 1))[0][0]
-    print(data)
-    return model1.predict(np.array(list(data.values())).reshape(1, -1))[0]
+    response_ = model1.predict(np.array(list(data.values())).reshape(1, -1))[0]
 
+    validate_output(response_)
+    return response_
 
 def predict_value(data):
     with open(params["model_training"]["order_of_columns"], "r") as file:
